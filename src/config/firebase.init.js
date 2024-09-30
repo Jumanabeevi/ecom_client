adf5b53d7cc0ee0e7609fd1ba235c69e7ffd93f5
Your Name <you@example.com>
2024-09-30 20:21:31 +0530

    first commit

---
+++ src/config/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAQXRGAYpwVNtyc4k9r0rHPqhdieYkEgJo",
+  authDomain: "yogaconnect-d555d.firebaseapp.com",
+  projectId: "yogaconnect-d555d",
+  storageBucket: "yogaconnect-d555d.appspot.com",
+  messagingSenderId: "344972606801",
+  appId: "1:344972606801:web:04b0fd3dff6c9afd6da770"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
